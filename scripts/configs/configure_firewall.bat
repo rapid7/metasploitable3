050a2fc316eec673e10f06203d67f2ba18a323a3
James Barnett <James_Barnett@rapid7.com>
2016-09-07 15:22:29 -0500

    Remove jmx vulnerability until licensing is figured out.

--- scripts/configs/configure_firewall.bat
+++ scripts/configs/configure_firewall.bat
@@ -5,4 +5,3 @@ netsh advfirewall firewall add rule name="Open Port 80 for IIS" dir=in action=al
 netsh advfirewall firewall add rule name="Open Port 4848 for GlassFish" dir=in action=allow protocol=TCP localport=4848
 netsh advfirewall firewall add rule name="Open Port 8080 for GlassFish" dir=in action=allow protocol=TCP localport=8080
 netsh advfirewall firewall add rule name="Open Port 3389 for Remote Desktop" dir=in action=allow protocol=TCP localport=3389
-netsh advfirewall firewall add rule name="Java 1.6 java.exe" dir=in action=allow program="C:\Program Files\Java\jdk1.6.0_26\bin\java.exe" enable=yes
