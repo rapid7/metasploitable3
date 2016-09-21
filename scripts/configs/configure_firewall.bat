502a642d321c21673a20d2da997a4d63b85e4adf
James Barnett <James_Barnett@rapid7.com>
2016-09-21 14:32:27 -0500

    Some touch ups to the WAMP install.

--- scripts/configs/configure_firewall.bat
+++ scripts/configs/configure_firewall.bat
@@ -5,3 +5,4 @@ netsh advfirewall firewall add rule name="Open Port 80 for IIS" dir=in action=al
 netsh advfirewall firewall add rule name="Open Port 4848 for GlassFish" dir=in action=allow protocol=TCP localport=4848
 netsh advfirewall firewall add rule name="Open Port 8080 for GlassFish" dir=in action=allow protocol=TCP localport=8080
 netsh advfirewall firewall add rule name="Open Port 3389 for Remote Desktop" dir=in action=allow protocol=TCP localport=3389
+netsh advfirewall firewall add rule name="Open Port 8585 for Wordpress and phpMyAdmin" dir=in action=allow protocol=TCP localport=8585
