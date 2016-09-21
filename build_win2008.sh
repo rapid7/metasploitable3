766ed9e53d04a807b853dd0c3be78a457ed9da7b
James Barnett <James_Barnett@rapid7.com>
2016-09-21 16:53:41 -0500

    Small text fixes to linux script.

--- build_win2008.sh
+++ build_win2008.sh
@@ -8,21 +8,21 @@ else
 fi
 
 if packer -v | grep -q '0.10.0'; then
-    echo 'Correct version of packer was found'
+    echo 'Compatible version of packer was found.'
 else
     echo "A compatible version of packer was not found. Please install from here: https://www.packer.io/downloads.html"
     exit 1
 fi
 
 if vagrant -v | grep -q '1.8.5'; then
-    echo 'Correct version of vagrant was found'
+    echo 'Correct version of vagrant was found.'
 else
     echo "A compatible version of vagrant was not found. At this time only 1.8.1 is supported. Please install from here: https://releases.hashicorp.com/vagrant/1.8.1/"
     exit 1
 fi
 
 if vagrant plugin list | grep -q 'vagrant-reload (0.0.1)'; then
-    echo 'Correct version of vagrant-reload plugin was found'
+    echo 'Compatible version of vagrant-reload plugin was found.'
 else
     echo "A compatible version of vagrant-reload plugin was not found."
     echo "Attempting to install..."
@@ -32,7 +32,7 @@ fi
 echo "All requirements found. Proceeding..."
 
 if ls | grep -q 'windows_2008_r2_virtualbox.box'; then
-    echo "It looks like the vagrant box has already been built."
+    echo "It looks like the vagrant box already exists. Skipping the Packer build."
 else
     echo "Building the Vagrant box..."
     packer build windows_2008_r2.json
@@ -41,10 +41,10 @@ fi
 echo "Attempting to add the box to Vagrant..."
 
 if vagrant box list | grep -q 'metasploitable3'; then
-    echo 'metasploitable3 already found in Vagrant box repository.'
+    echo 'metasploitable3 already found in Vagrant box repository. Skipping the addition to Vagrant.'
 else
     vagrant add windows_2008_r2_virtualbox.box --name metasploitable3
-    echo "Box successfully added to vagrant."
+    echo "Box successfully added to Vagrant."
 fi
 
 echo "SUCCESS: Run 'vagrant up' to provision and start metasploitable3."
