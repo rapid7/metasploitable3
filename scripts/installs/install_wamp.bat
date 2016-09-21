c4fce7f21b6fdf3ab689c0c7526cbbd2832f32bf
James Barnett <James_Barnett@rapid7.com>
2016-09-21 14:32:27 -0500

    Some touch ups to the WAMP install.

--- scripts/installs/install_wamp.bat
+++ scripts/installs/install_wamp.bat
@@ -5,5 +5,5 @@ copy /Y "C:\vagrant\resources\wamp\wampserver2.2d-x64.exe" "C:\Windows\Temp\wamp
 C:\Windows\Temp\wampserver2.2.d-x64.exe /verysilent
 copy /Y "C:\vagrant\resources\wamp\httpd.conf" "C:\wamp\bin\apache\Apache2.2.21\conf\httpd.conf"
 copy /Y "C:\vagrant\resources\wamp\phpmyadmin.conf" "C:\wamp\alias\phpmyadmin.conf"
-net start wampapache
-net start wampmysqld
+sc config wampapache start= auto
+sc config wampmysqld start= auto
