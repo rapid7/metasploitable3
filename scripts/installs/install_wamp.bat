c6aef8d96d08fce7c76c69edeb3d75ec54e43562
wchen-r7 <wei_chen@rapid7.com>
2016-09-26 17:17:12 -0500

    Fix wamp download link

--- scripts/installs/install_wamp.bat
+++ scripts/installs/install_wamp.bat
@@ -1,6 +1,7 @@
 REM The below command was temporarily working to successfully download the installer but is now giving a 404.
 REM Switching to copying the file from the repo until a reliable host can be found.
-REM powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/wampserver/WampServer%202/WampServer%202.2/wampserver2.2d-x64.exe', 'C:\Windows\Temp\wampserver2.2.d-x64.exe')" <NUL
+REM http://sourceforge.mirrorservice.org/w/wa/wampserver/WampServer%202/WampServer%202.2/wampserver2.2d-x64.exe
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://sourceforge.mirrorservice.org/w/wa/wampserver/WampServer 2/WampServer 2.2/wampserver2.2d-x64.exe', 'C:\Windows\Temp\wampserver2.2.d-x64.exe')" <NUL
 copy /Y "C:\vagrant\resources\wamp\wampserver2.2d-x64.exe" "C:\Windows\Temp\wampserver2.2.d-x64.exe"
 C:\Windows\Temp\wampserver2.2.d-x64.exe /verysilent
 copy /Y "C:\vagrant\resources\wamp\httpd.conf" "C:\wamp\bin\apache\Apache2.2.21\conf\httpd.conf"
