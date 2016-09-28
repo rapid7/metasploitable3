4fdd52f5d34d77bf5e5d2cb54d92545e1a8e58c2
wchen-r7 <wei_chen@rapid7.com>
2016-09-28 04:05:09 -0500

    use the correct path

--- resources/wordpress/update_wp_db.bat
+++ resources/wordpress/update_wp_db.bat
@@ -2,4 +2,4 @@ powershell -File "C:\Program Files\wordpress\update_ip.ps1"
 cmd /c ""C:\wamp\bin\mysql\mysql5.5.20\bin\mysql.exe" -u root --password="" -e "drop database wordpress;" "
 cmd /c ""C:\wamp\bin\mysql\mysql5.5.20\bin\mysql.exe" -u root --password="" -e "create database wordpress;" "
 cmd /c ""C:\wamp\bin\mysql\mysql5.5.20\bin\mysql.exe" -u root --password=""  wordpress < "C:\Program Files\wordpress\wordpress.sql""
-del wordpress.sql
+del "C:\Program Files\wordpress\wordpress.sql"
