b0112a96178a3dbe87249aa9ed1efd66aa738ee8
wchen-r7 <wei_chen@rapid7.com>
2016-09-29 16:30:31 -0500

    auto update wordpress IP

--- scripts/installs/install_wordpress.bat
+++ scripts/installs/install_wordpress.bat
@@ -1,7 +1,8 @@
 mkdir "C:\Program Files\wordpress"
 copy C:\vagrant\resources\wordpress\update_ip.ps1 "C:\Program Files\wordpress\update_ip.ps1"
-copy C:\vagrant\resources\wordpress\update_wp_db.bat "C:\Program Files\wordpress\update_wp_db.bat"
 cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\vagrant\resources\wordpress\wordpress.zip -oC:\wamp\www\"
-cmd /c ""C:\Program Files\wordpress\update_wp_db.bat""
-schtasks /create /tn "update_wp_db" /tr "\"C:\Program Files\wordpress\update_wp_db.bat\"" /sc onstart /NP /ru "SYSTEM"
+cmd /c ""C:\wamp\bin\mysql\mysql5.5.20\bin\mysql.exe" -u root --password=""  -e "create database wordpress;""
+cmd /c ""C:\wamp\bin\mysql\mysql5.5.20\bin\mysql.exe" -u root --password=""  wordpress < "C:\Vagrant\resources\wordpress\wordpress.sql""
+schtasks /create /tn "update_wp_db" /tr "\"cmd.exe\" /c powershell -File \"C:\Program Files\wordpress\update_ip.ps1\"" /sc onstart /NP /ru "SYSTEM"
+powershell -File "C:\Program Files\wordpress\update_ip.ps1"
 attrib -r +s C:\wamp\www\wordpress
