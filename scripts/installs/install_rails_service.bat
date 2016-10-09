cbf8765072a0eb811e493cef2a51fcaeaba5f987
wchen-r7 <wei_chen@rapid7.com>
2016-10-09 02:45:24 -0500

    Start rails task

--- scripts/installs/install_rails_service.bat
+++ scripts/installs/install_rails_service.bat
@@ -1,2 +1,3 @@
 copy C:\Vagrant\resources\rails_server\start_rails_server.bat "C:\Program Files\Rails_Server"
 schtasks /create /tn "rails" /tr "\"cmd.exe\" /c \"C:\Program Files\Rails_Server\start_rails_server.bat\"" /sc onstart /NP /ru "SYSTEM"
+schtasks /Run /TN rails
