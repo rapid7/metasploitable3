f10eed5e70ef2a28012f7c682d2153fc5a98b4a1
wchen-r7 <wei_chen@rapid7.com>
2016-10-19 15:34:32 -0500

    Use only httpd-dav.conf

--- scripts/installs/setup_webdav.bat
+++ scripts/installs/setup_webdav.bat
@@ -1,5 +1,4 @@
 net stop wampapache
 mkdir C:\wamp\www\uploads\
-copy /Y C:\Vagrant\resources\webdav\httpd.conf C:\wamp\bin\apache\Apache2.2.21\conf\
-copy /Y C:\Vagrant\resources\webdav\httpd-dav.conf C:\wamp\bin\apache\Apache2.2.21\conf\extra\
+copy /Y C:\Vagrant\resources\webdav\httpd-dav.conf C:\wamp\alias
 net start wampapache
