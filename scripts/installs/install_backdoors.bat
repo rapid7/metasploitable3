5711ad8e29274cdf3a568dc35e5ad2bbe3f2298b
wchen-r7 <wei_chen@rapid7.com>
2016-10-25 14:48:21 -0500

    Add common backdoors

---
+++ scripts/installs/install_backdoors.bat
@@ -0,0 +1,3 @@
+copy C:\vagrant\resources\backdoors\caidao.asp "C:\inetpub\wwwroot"
+copy C:\vagrant\resources\backdoors\mma.php "C:\wamp\www"
+copy C:\vagrant\resources\backdoors\meterpreter.php "C:\wamp\www"
