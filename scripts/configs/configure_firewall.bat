9353c190ef6966492f2db080c2d07b0db464454d
wchen-r7 <wei_chen@rapid7.com>
2016-10-28 11:41:53 -0500

    Block MySQL too

--- scripts/configs/configure_firewall.bat
+++ scripts/configs/configure_firewall.bat
@@ -14,3 +14,4 @@ netsh advfirewall firewall add rule name="Closed port 445 for SMB" dir=in action
 netsh advfirewall firewall add rule name="Closed port 139 for NetBIOS" dir=in action=block protocol=TCP localport=139
 netsh advfirewall firewall add rule name="Closed port 135 for NetBIOS" dir=in action=block protocol=TCP localport=135
 netsh advfirewall firewall add rule name="Closed Port 3389 for Remote Desktop" dir=in action=block protocol=TCP localport=3389
+netsh advfirewall firewall add rule name="Closed Port 3306 for MySQL" dir=in action=block protocol=TCP localport=3306
