3632b89b31c08245443cf640f57e3acdf4a4875c
wchen-r7 <wei_chen@rapid7.com>
2016-11-03 22:55:08 -0500

    Make ManageEngine run as LOCAL SERVICE

--- scripts/installs/install_manageengine.bat
+++ scripts/installs/install_manageengine.bat
@@ -1,2 +1,13 @@
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://archives.manageengine.com/desktop-central/91084/ManageEngine_DesktopCentral.exe', 'C:\Windows\Temp\ManageEngine_DesktopCentral.exe')" <NUL
 start /WAIT C:\Windows\Temp\ManageEngine_DesktopCentral.exe /w /s /f1C:\Vagrant\resources\manageengine\setup.iss
+net stop "ManageEngine Desktop Central Server"
+net stop "MEDC Server Component - Apache"
+net stop "MEDC Server Component - Notification Server"
+icacls "C:\ManageEngine" /grant "NT Authority\LOCAL SERVICE:(OI)(CI)F" /T
+sc config "DesktopCentralServer" obj= "NT Authority\LOCAL SERVICE"
+sc config "MEDC Server Component - Notification Server" obj= "NT Authority\LOCAL SERVICE"
+sc config "MEDCServerComponent-Apache" obj= "NT Authority\LOCAL SERVICE"
+net start "MEDC Server Component - Apache"
+net start "MEDC Server Component - Notification Server"
+net start "ManageEngine Desktop Central Server"
+
