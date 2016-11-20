8782ce563b440fedcfdcbdd0758d8c47ffa82762
Nicholas Charron <ncharron@gmail.com>
2016-11-20 11:20:14 -0500

    Added windows-shell provisioning along with inline powershell for choco and some restarts

--- scripts/installs/install_netfx.ps1
+++ scripts/installs/install_netfx.ps1
@@ -1,14 +1,7 @@
 # setup dotnetfx4
 $netfx_url = "https://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe"
 
-
 Write-Output "Downloading $netfx_url"
 (New-Object System.Net.WebClient).DownloadFile($netfx_url, "C:\Windows\Temp\dotNetFx40_Full_x86_x64.exe")
 Write-Output "Starting Install of dotNetFx40_Full_x86_x64.exe"
 Start-Process "C:\Windows\Temp\dotNetFx40_Full_x86_x64.exe" "/q /norestart" -Wait
-
-$ChocoInstallPath = "$env:SystemDrive\ProgramData\Chocolatey\bin"
-
-if (!(Test-Path $ChocoInstallPath)) {
-  iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-}
