184d3be6c31d7e36ec46fe1048c5d95d3a654aaf
James Barnett <James_Barnett@rapid7.com>
2017-02-13 15:53:51 -0600

    WIP: Adding Linux VM using chef solo provisioning

---
+++ chef/cookbooks/metasploitable/.delivery/build_cookbook/recipes/lint.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: build_cookbook
+# Recipe:: lint
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'delivery-truck::lint'
---
+++ chef/cookbooks/metasploitable/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: metasploitable
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
