bb6e4bd67d5d57b7976aef1f0b259656b1c9f0a7
wchen-r7 <wei_chen@rapid7.com>
2017-04-07 14:27:54 -0500

    Update bat

--- scripts/installs/setup_linux_share.bat
+++ scripts/installs/setup_linux_share.bat
@@ -1,4 +1,6 @@
 @echo off
 ping -n 1 -4 172.28.128.3 | find /i "TTL=" > nul
-IF ERRORLEVEL 1 (echo "Linux host not available.") ELSE (net use W: \\172.28.128.3\public /user:chewbacca rwaaaaawr5)
-
+IF ERRORLEVEL 1 (echo "Linux host not available.") ELSE (
+  cmdkey /add:172.28.128.3 /user:chewbacca /pass:rwaaaaawr5
+  net use W: \\172.28.128.3\public /savecred /p:yes
+)
