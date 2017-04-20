ac473a966e2c56d176996b8999c0fbf80f299a04
James Barnett <James_Barnett@rapid7.com>
2017-04-20 13:43:11 -0500

    Fix bundle install and startup errors.

--- chef/cookbooks/metasploitable/recipes/readme_app.rb
+++ chef/cookbooks/metasploitable/recipes/readme_app.rb
@@ -9,6 +9,7 @@
 include_recipe 'metasploitable::ruby23'
 
 package 'git'
+package 'nodejs'
 
 directory '/opt/readme_app' do
   mode '0777'
@@ -18,16 +19,20 @@ bash "clone the readme app and install gems" do
   code <<-EOH
     cd /opt/
     git clone https://github.com/jbarnett-r7/metasploitable3-readme.git readme_app
-    cd readme_app
-    bundle install
   EOH
 end
 
-cookbook_file '/etc/init.d/readme_app' do
-  source 'readme_app/readme_app'
-  mode '760'
+cookbook_file '/opt/readme_app/start.sh' do
+  source 'readme_app/start.sh'
+  mode '0777'
+end
+
+cookbook_file '/etc/init/readme_app.conf' do
+  source 'readme_app/readme_app.conf'
+  mode '0777'
 end
 
 service 'readme_app' do
+  supports restart: false, start: true, reload: false, status: false
   action [:enable, :start]
 end
