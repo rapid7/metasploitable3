33d0a66011fa0a8ceb2e00d8e9f0f55b5740cc6c
James Barnett <James_Barnett@rapid7.com>
2017-07-06 14:52:39 -0500

    Configure other services in iptables

--- chef/cookbooks/metasploitable/attributes/default.rb
+++ chef/cookbooks/metasploitable/attributes/default.rb
@@ -8,3 +8,13 @@ default[:metasploitable][:docker_users] = ['boba_fett',
                                              'chewbacca',]
 
 default[:metasploitable][:files_path] = '/vagrant/chef/cookbooks/metasploitable/files/'
+
+default[:metasploitable][:ports][:cups] = 631
+default[:metasploitable][:ports][:apache] = 80
+default[:metasploitable][:ports][:unrealircd] = 6697
+default[:metasploitable][:ports][:proftpd] = 21
+default[:metasploitable][:ports][:mysql] = 3306
+default[:metasploitable][:ports][:chatbot][:ui] = default[:metasploitable][:ports][:apache]
+default[:metasploitable][:ports][:chatbot][:nodejs] = 3000
+default[:metasploitable][:ports][:chatbot][:ruby] = 8181
+default[:metasploitable][:ports][:samba] = 445
