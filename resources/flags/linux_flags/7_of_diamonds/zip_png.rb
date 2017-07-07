61c8130244e561dfbc094198ff7c204f150e1297
wchen-r7 <wei_chen@rapid7.com>
2017-07-07 11:58:46 -0500

    All the scripts needed to build and extract Linux flags

---
+++ resources/flags/linux_flags/7_of_diamonds/zip_png.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+# gem install rubyzip
+require 'zip'
+
+SOURCEPNG = 'source.png'
+CARDNAME  = '7_of_diamonds.png'
+ZIP_NAME  = '7_of_diamonds.zip'
+password = ARGV.shift
+
+if password.nil? || password.empty?
+  puts "[x] Please set a password for the zip file you're trying to create"
+  exit
+end
+
+data = File.read(SOURCEPNG)
+zip = Zip::OutputStream.write_buffer(::StringIO.new(''), Zip::TraditionalEncrypter.new(password)) do |o|
+  o.put_next_entry(CARDNAME)
+  o.write data
+end
+
+File.open(ZIP_NAME, 'wb') do |f|
+  f.write(zip.string)
+end
+
+puts "[*] #{ZIP_NAME} created with password: #{password}"
