a7a0a0edaf643759fcd9a11294f871b40eb2d7df
wchen-r7 <wei_chen@rapid7.com>
2017-07-12 14:58:29 -0500

    Delete files that are not needed anymore

--- chef/cookbooks/metasploitable/recipes/sinatra.rb
+++ chef/cookbooks/metasploitable/recipes/sinatra.rb
@@ -13,7 +13,7 @@ directory '/opt/sinatra' do
   mode '0777'
 end
 
-['Gemfile', 'README.txt', 'check.rb', 'poc.rb', 'start.sh', 'server.rb'].each do |fname|
+['Gemfile', 'README.txt', 'start.sh', 'server.rb'].each do |fname|
   cookbook_file "/opt/sinatra/#{fname}" do
     source "sinatra/#{fname}"
     mode '0777'
