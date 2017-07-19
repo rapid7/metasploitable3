6acb359c09b4712caf102d03bdbe4720459adb79
Chan9390 <bnchandrapal@gmail.com>
2017-07-19 17:42:49 +0530

    Updated mysql.rb

--- chef/cookbooks/metasploitable/test/windows/mysql.rb
+++ chef/cookbooks/metasploitable/test/windows/mysql.rb
@@ -2,11 +2,13 @@ control "mysql" do
   title "MySQL"
   desc "Check if MySQL is running properly. Installation script available at /scripts/installs/setup_mysql.bat"
 
-  describe command('sc query wampmysqld') do
-   its('stdout') { should match "STATE              : 4  RUNNING" }
+  describe service('wampmysqld') do
+   it { should be_installed }
+   it { should be_enabled }
+   it { should be_running }
   end
 
-  describe command('netstat -aob | findstr :3306') do
-   its('stdout') { should match "LISTENING" }
+  describe port('3306') do
+   it { should be_listening }
   end
 end
