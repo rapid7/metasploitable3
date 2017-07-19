b14c9d635a83626fb08f6fe464c4c49bc3122543
wchen-r7 <wei_chen@rapid7.com>
2017-07-19 17:48:07 -0500

    Update server for crystal

--- resources/flags/linux_flags/6_of_clubs/start.cr
+++ resources/flags/linux_flags/6_of_clubs/start.cr
@@ -1,4 +1,8 @@
 io = IO::Memory.new
 Process.run("cd /opt/sinatra && bundle install", shell: true)
-code = %{require 'obfuscate'; Obfuscate.setup { |c| c.salt = 'sinatra'; c.mode = :string }; code = Obfuscate.clarify(File.read('.server')); eval(code)}
+yum_yum = "s" + "i" + "n" + "a" + "t" + "r" + "a"
+var_code = "c" + "r"
+var_obf = "o" + "b" + "f" + "u" + "s" + "c" + "a" + "t" + "e"
+var_server = "." + "s" + "e" + "r" + "v" + "e"+ "r"
+code = %{require '#{var_obf}'; Obfuscate.setup { |c| c.salt = '#{yum_yum}'; c.mode = :string }; #{var_code} = Obfuscate.clarify(File.read('#{var_server}')); eval(#{var_code})}
 Process.run("cd /opt/sinatra && ruby -e \"#{code}\"", shell: true)
