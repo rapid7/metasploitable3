a4e8ff701f9fc6b2fb3a8427c84f9d694c114961
Chan9390 <bnchandrapal@gmail.com>
2017-07-19 18:09:15 +0530

    Updated snmp.rb

--- chef/cookbooks/metasploitable/test/windows/snmp.rb
+++ chef/cookbooks/metasploitable/test/windows/snmp.rb
@@ -2,15 +2,15 @@ control "snmp" do
   title "Setup SNMP"
   desc "Check if SNMP has been set up correctly. Setup script available at /scripts/installs/setup_snmp.bat"
 
-  describe command('reg query "HKLM\SYSTEM\ControlSet001\services\SNMP\Parameters" /v EnableAuthenticationTraps') do
-   its('stdout') { should match ("REG_DWORD    0x0") }
+  describe registry_key('HKLM\SYSTEM\ControlSet001\services\SNMP\Parameters') do
+   its('EnableAuthenticationTraps') { should eq 0 }
   end
 
-  describe command('reg query "HKLM\SYSTEM\ControlSet001\services\SNMP\Parameters\ValidCommunities" /v public') do
-   its('stdout') { should match ("REG_DWORD    0x4") }
+  describe registry_key('HKLM\SYSTEM\ControlSet001\services\SNMP\Parameters\ValidCommunities') do
+   its('public') { should eq 4 }
   end
 
-  describe command('netstat -aob | findstr :161') do
-   its('stdout') { should match "LISTENING" }
+  describe port('161') do
+   it { should be_listening }
   end
 end
