52fd8f351d5de26ffa73285d6d76cd80e56f0657
wchen-r7 <wei_chen@rapid7.com>
2017-07-20 14:08:03 -0500

    Use Crystal loader for Sinatra

--- chef/cookbooks/metasploitable/files/sinatra/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /opt/sinatra
-bundle install
-ruby -e "require 'obfuscate'; Obfuscate.setup { |c| c.salt = 'sinatra'; c.mode = :string}; code = Obfuscate.clarify(File.read('server')); eval(code)"
