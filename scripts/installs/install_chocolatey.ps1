f68f1cb3358fda29e9d8ae93ebd2a394f54c2b6d
Chan9390 <bnchandrapal@gmail.com>
2017-07-27 20:58:06 +0530

    Removed unwanted certificates and chocolatey installation scripts

--- scripts/installs/install_chocolatey.ps1
+++
@@ -1,5 +0,0 @@
-$ChocoInstallPath = "$env:SystemDrive\ProgramData\Chocolatey\bin"
-
-if (!(Test-Path $ChocoInstallPath)) {
-  iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-}
