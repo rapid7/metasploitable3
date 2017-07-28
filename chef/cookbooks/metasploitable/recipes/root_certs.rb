43be6e47f42cca766855c14ca064afc244a92540
Chan9390 <bnchandrapal@gmail.com>
2017-07-28 18:29:21 +0530

    Squashed commit of the following:
    commit 9d6666fbb9f066179e17030808bd02e3ad27a2c0
    Author: Chan9390 <bnchandrapal@gmail.com>
    Date:   Fri Jul 28 18:20:46 2017 +0530
    
        Removed cmd /c from commands
    
    commit ef6abe66f4a79e150c9d85a0a38896b061bc5def
    Author: Chan9390 <bnchandrapal@gmail.com>
    Date:   Fri Jul 28 07:14:52 2017 +0530
    
        Added certificates

--- chef/cookbooks/metasploitable/recipes/root_certs.rb
+++ chef/cookbooks/metasploitable/recipes/root_certs.rb
@@ -3,3 +3,39 @@
 # Recipe:: root_certs
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+execute 'addtrust_external_ca' do
+  command 'certutil -addstore -f "Root" A:\addtrust_external_ca.cer'
+end
+
+execute 'baltimore_ca' do
+  command 'certutil -addstore -f "Root" A:\baltimore_ca.cer'
+end
+
+execute 'digicert' do
+  command 'certutil -addstore -f "Root" A:\digicert.cer'
+end
+
+execute 'equifax' do
+  command 'certutil -addstore -f "Root" A:\equifax.cer'
+end
+
+execute 'globalsign' do
+  command 'certutil -addstore -f "Root" A:\globalsign.cer'
+end
+
+execute 'gte_cybertrust' do
+  command 'certutil -addstore -f "Root" A:\gte_cybertrust.cer'
+end
+
+execute 'microsoft_root_2011' do
+  command 'certutil -addstore -f "Root" A:\microsoft_root_2011.cer'
+end
+
+execute 'thawte_primary_root' do
+  command 'certutil -addstore -f "Root" A:\thawte_primary_root.cer'
+end
+
+execute 'utn-userfirst' do
+  command 'certutil -addstore -f "Root" A:\utn-userfirst.cer'
+end
