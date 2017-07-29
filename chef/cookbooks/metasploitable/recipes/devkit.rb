22789a61b7fce628ecaf992592cedaa641265a86
Chan9390 <bnchandrapal@gmail.com>
2017-07-29 21:35:15 +0530

    Install DevKit

---
+++ chef/cookbooks/metasploitable/recipes/devkit.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook:: metasploitable
+# Recipe:: devkit
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+directory 'C:\Program Files\Rails_Server' do
+  action :create
+end
+
+directory 'C:\Program Files\Rails_Server\devkit' do
+  action :create
+end
+
+powershell_script "Download DevKit" do
+  code "(New-Object System.Net.WebClient).DownloadFile('http://dl.bintray.com/oneclick/rubyinstaller/DevKit-mingw64-64-4.7.2-20130224-1432-sfx.exe', 'C:\\Program Files\\Rails_Server\\devkit\\devkit.exe')"
+end
+
+batch 'Extract DevKit' do
+  code <<-EOH
+    "C:\\Program Files\\7-Zip\\7z.exe" x "C:\\Program Files\\Rails_Server\\devkit\\devkit.exe" -o"C:\\Program Files\\Rails_Server\\devkit\\"
+  EOH
+end
+
+execute 'Copy files' do
+  command 'copy /Y C:\Vagrant\resources\Rails_Server\devkit\dk.rb "C:\\Program Files\\Rails_Server\\devkit"'
+end
+
+batch 'Install DevKit' do
+  code <<-EOH    
+    C:\\tools\\ruby23\\bin\\ruby.exe "C:\\Program Files\\Rails_Server\\devkit\\dk.rb" init
+    C:\\tools\\ruby23\\bin\\ruby.exe "C:\\Program Files\\Rails_Server\\devkit\\dk.rb" install
+    "C:\\Program Files\\Rails_Server\\devkit\\devkitvars.bat"
+  EOH
+end
+
