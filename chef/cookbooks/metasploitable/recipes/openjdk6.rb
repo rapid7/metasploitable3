7ceab051131d3e2d34e58dd4df17882a8b7fadac
Chan9390 <bnchandrapal@gmail.com>
2017-07-29 18:20:27 +0530

    Squashed commit of the following:    - Updated command    - Added OpenJDK

---
+++ chef/cookbooks/metasploitable/recipes/openjdk6.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: metasploitable
+# Recipe:: openjdk6
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+powershell_script 'Download OpenJDK' do
+  code '(New-Object System.Net.WebClient).DownloadFile(\'https://github.com/downloads/alexkasko/openjdk-unofficial-builds/openjdk-1.6.0-unofficial-b27-windows-amd64.zip\', \'C:\Windows\Temp\openjdk-1.6.0-unofficial-b27-windows-amd64.zip\')'
+end
+
+batch 'Install OpenJDK' do
+  code '"C:\Program Files\7-Zip\7z.exe" x "C:\Windows\Temp\openjdk-1.6.0-unofficial-b27-windows-amd64.zip" -oC:\openjdk6"'
+end
+
