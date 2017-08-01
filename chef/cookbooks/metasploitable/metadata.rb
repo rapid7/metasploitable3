d91f8b3ffe8f6261ac112ca9f853cc1f02c2c9f5
Chan9390 <bnchandrapal@gmail.com>
2017-08-01 19:16:15 +0530

    Updated metadata.rb

--- chef/cookbooks/metasploitable/metadata.rb
+++ chef/cookbooks/metasploitable/metadata.rb
@@ -1,20 +1,19 @@
 name 'metasploitable'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures metasploitable'
-long_description 'Installs/Configures metasploitable'
+maintainer 'Rapid7'
+maintainer_email ''
+license 'BSD-3-clause'
+description 'Installs/Configures metasploitable3'
+long_description 'Installs/Configures metasploitable3'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/metasploitable/issues'
+# issues_url 'https://github.com/rapid7/metasploitable/issues'
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/metasploitable'
+# source_url 'https://github.com/rapid7/metasploitable'
