a588e8ace3cc8153e3a8181bb1e7ffef704d3a36
Chan9390 <bnchandrapal@gmail.com>
2017-08-11 21:58:10 +0530

    Corrected mistake in ftp recipe

--- chef/cookbooks/metasploitable/recipes/ftp.rb
+++ chef/cookbooks/metasploitable/recipes/ftp.rb
@@ -5,6 +5,6 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 cookbook_file 'C:\Windows\System32\inetsrv\config\applicationHost.config' do
-  source 'iis\applicationHost.config'
+  source 'iis/applicationHost.config'
   action :create
 end
