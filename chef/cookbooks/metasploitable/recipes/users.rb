afd863afe1950364e7d4413ebc0375849b84505e
James Barnett <James_Barnett@rapid7.com>
2017-08-15 17:28:53 -0500

    Change user passwords and groups

--- chef/cookbooks/metasploitable/recipes/users.rb
+++ chef/cookbooks/metasploitable/recipes/users.rb
@@ -4,8 +4,6 @@
 #
 # Copyright:: 2017, Rapid7, All Rights Reserved.
 
-# See scripts/configs/create_users.bat for passwords
-
 uid = 1111
 
 node[:users].each do |u, attributes|
@@ -19,3 +17,13 @@ node[:users].each do |u, attributes|
   end
   uid += 1
 end
+
+administrator_members = node[:users].keys.find_all { |user| node[:users][user][:admin] == true }
+
+group 'sudo' do
+  action :modify
+  members administrator_members.map { |u| node[:users][u][:username] }
+  append true
+end
+
+
