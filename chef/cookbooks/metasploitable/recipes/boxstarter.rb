28e6e76632a57fe85fcb9e10240041a15279b3b5
Chan9390 <bnchandrapal@gmail.com>
2017-08-28 18:12:00 +0530

    Replaced batch resource to choco in BoxStarter recipe

--- chef/cookbooks/metasploitable/recipes/boxstarter.rb
+++ chef/cookbooks/metasploitable/recipes/boxstarter.rb
@@ -4,10 +4,6 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-batch 'Install Boxstarter' do
-  code <<-EOH
-    chocolatey feature enable -n=allowGlobalConfirmation
-    choco install BoxStarter
-    chocolatey feature disable -n=allowGlobalConfirmation
-    EOH
+chocolatey_package 'BoxStarter' do
+  action :install
 end
