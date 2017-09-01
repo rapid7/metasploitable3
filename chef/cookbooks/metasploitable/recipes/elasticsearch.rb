12c5eb2f0b83bd3d2141765722792505f8523e0f
Chan9390 <bnchandrapal@gmail.com>
2017-09-01 18:47:28 +0530

    Updated elasticsearch.rb

--- chef/cookbooks/metasploitable/recipes/elasticsearch.rb
+++ chef/cookbooks/metasploitable/recipes/elasticsearch.rb
@@ -4,6 +4,9 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+include_recipe 'metasploitable::7zip'
+include_recipe 'metasploitable::jdk8'
+
 powershell_script 'Download ElasticSearch' do
   code '(New-Object System.Net.WebClient).DownloadFile(\'http://repo1.maven.org/maven2/org/elasticsearch/elasticsearch/1.1.1/elasticsearch-1.1.1.zip\', \'C:\Windows\Temp\elasticsearch-1.1.1.zip\')'
 end
@@ -29,24 +32,33 @@ execute 'Starting service' do
   action :run
 end
 
-powershell_script 'Sleep 30 secs' do
-  code 'Start-Sleep -s 30'
+ruby_block 'Sleep for 30 secs' do
+  block do
+    sleep(30)
+  end
+  action :run
 end
 
 windows_service 'elasticsearch-service-x64' do
   action :start
 end
 
-powershell_script 'Sleep 5 secs' do
-  code 'Start-Sleep -s 5'
+ruby_block 'Sleep for 15 secs' do
+  block do
+    sleep(15)
+  end
+  action :run
 end
 
 powershell_script 'Test response' do
   code "$req = [System.Net.HttpWebRequest]::Create('http://localhost:9200/metasploitable3/'); $req.method = 'PUT'; $req.GetResponse()"
 end
 
-powershell_script 'Sleep 5 secs' do
-  code 'Start-Sleep -s 5'
+ruby_block 'Sleep for 15 secs' do
+  block do
+    sleep(15)
+  end
+  action :run
 end
 
 powershell_script 'Test' do
