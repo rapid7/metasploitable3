dc43bc6c9f04c3949749af4d61119048a2fbe421
Stan <stan@fullstackinfo.com>
2018-12-01 19:21:52 -0800

    add explicit TLS 1.2 setting to PS session when downloading RubyDevKit from new https endpoint

--- scripts/installs/install_devkit.bat
+++ scripts/installs/install_devkit.bat
@@ -1,5 +1,5 @@
 mkdir "C:\RubyDevKit"
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://dl.bintray.com/oneclick/rubyinstaller/DevKit-mingw64-64-4.7.2-20130224-1432-sfx.exe', 'C:\RubyDevKit\devkit.exe')" <NUL
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://dl.bintray.com/oneclick/rubyinstaller/DevKit-mingw64-64-4.7.2-20130224-1432-sfx.exe', 'C:\RubyDevKit\devkit.exe')" <NUL
 cmd /c ""C:\Program Files\7-Zip\7z.exe" x "C:\RubyDevKit\devkit.exe" -o"C:\RubyDevKit\""
 copy /Y C:\Vagrant\resources\Rails_Server\devkit\dk.rb "C:\RubyDevKit"
 C:\tools\ruby23\bin\ruby.exe "C:\RubyDevKit\dk.rb" init
