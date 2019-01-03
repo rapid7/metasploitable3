25b828cc2ba3f50f4f29cf7a97e3de296196f2da
Nicholas Kasprinski <nicholaskasprinski@gmail.com>
2019-01-02 17:26:44 -0800

    remove install iis script as the os setup will do the intitial setup

--- scripts/installs/setup_iis.bat
+++
@@ -1,1 +0,0 @@
-start /w PKGMGR.EXE /quiet /norestart /iu:IIS-WebServerRole;IIS-WebServer;IIS-CommonHttpFeatures;IIS-ApplicationDevelopment;IIS-ASPNET;IIS-NetFxExtensibility;IIS-ASP;IIS-CGI;IIS-ISAPIExtensions;IIS-ISAPIFilter;IIS-ServerSideIncludes;IIS-FTPServer;IIS-FTPSvc;IIS-FTPExtensibility;
