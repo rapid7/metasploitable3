49f6ada4d89211c5d2ff316496f6fe5b7fe30c12
Dave Eargle <dave@daveeargle.com>
2020-10-02 17:08:16 -0400

    allow all loopback traffic
    see 
    https://github.com/rapid7/metasploitable3/pull/458#issuecomment-702942817

--- chef/cookbooks/metasploitable/recipes/iptables.rb
+++ chef/cookbooks/metasploitable/recipes/iptables.rb
@@ -10,6 +10,14 @@ iptables_rule '00_established' do
   lines '-A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT'
 end
 
+iptables_rule '00_lo_allow_in' do
+  lines '-I INPUT -i lo -j ACCEPT'
+end
+
+iptables_rule '00_lo_allow_out' do
+  lines '-I OUTPUT -o lo -j ACCEPT'
+end
+
 iptables_rule '01_ssh' do
   lines "-A INPUT -p tcp --dport 22 -j ACCEPT"
 end
@@ -17,6 +25,3 @@ end
 iptables_rule '999_drop_all' do
   lines '-A INPUT -j DROP'
 end
-
-
-
