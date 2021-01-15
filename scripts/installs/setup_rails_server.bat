9bb95f1a722352ab2e836b0621d446a4df42d62f
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2021-01-15 17:14:06 -0600

    also enforce bundler version in setup

--- scripts/installs/setup_rails_server.bat
+++ scripts/installs/setup_rails_server.bat
@@ -1,3 +1,4 @@
 copy /Y C:\Vagrant\resources\rails_server\Gemfile "C:\Program Files\Rails_Server"
 cd "C:\Program Files\Rails_Server"
-C:\tools\ruby23\bin\bundler.bat install
+gem install bundler -v '1.17.3' --no-document
+bundle install
