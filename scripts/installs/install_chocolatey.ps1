b9d61f483046e20f63972e65eb043b17cacf0694
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2021-01-15 10:50:06 -0600

    enable tls12 for vagrant key download

--- scripts/installs/install_chocolatey.ps1
+++
@@ -1,6 +0,0 @@
-$ChocoInstallPath = "$env:SystemDrive\ProgramData\Chocolatey\bin"
-
-if (!(Test-Path $ChocoInstallPath)) {
-  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-  iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-}
