d277af9007188a66a015912867264c4618592c7d
Jeffrey Martin <jmartin@op3n4m3.dev>
2024-04-20 19:18:39 -0500

    additional update for openjdk

--- chef/cookbooks/metasploitable/test/windows/openjdk6.rb
+++ chef/cookbooks/metasploitable/test/windows/openjdk6.rb
@@ -2,11 +2,11 @@ control "openjdk6" do
   title "Check OpenJDK 6"
   desc "Checks for the installation of OpenJDK 6. Setup file available at /scripts/installs/setup_openjdk6.bat"
 
-  describe file('C:\\openjdk6\\openjdk-1.6.0-unofficial-b27-windows-amd64') do
+  describe file('C:\\openjdk6\\openjdk-1.6.0-unofficial-b28-windows-amd64-installer') do
    it { should exist }
   end
 
-  describe command('C:\openjdk6\openjdk-1.6.0-unofficial-b27-windows-amd64\jre\bin\java.exe -version') do
+  describe command('C:\openjdk6\openjdk-1.6.0-unofficial-b28-windows-amd64-installer\jre\bin\java.exe -version') do
    its('stdout') { should match "openjdk version \"1.6.0-unofficial\"" }
   end
 end
