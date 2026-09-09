4c40c06c47fac3ec56f2ac93cd9035f7c5f6fed3
Blaz <blaz.pirc04@gmail.com>
2026-09-09 10:11:39 +0200

    changed service name of jenkins to jinkies

--- scripts/installs/setup_jenkins.bat
+++ scripts/installs/setup_jenkins.bat
@@ -2,5 +2,5 @@ mkdir "%ProgramFiles%\jenkins"
 copy C:\vagrant\resources\jenkins\jenkins.war "%ProgramFiles%\jenkins"
 copy C:\vagrant\resources\jenkins\jenkins.exe "%ProgramFiles%\jenkins"
 copy C:\vagrant\resources\jenkins\jenkins.xml "%ProgramFiles%\jenkins"
-"%ProgramFiles%\jenkins\jenkins.exe" -Service Install
+"%ProgramFiles%\jenkins\jenkins.exe" -Service Install -ServiceName jinkies -ServiceDisplayName "jinkies" -ServiceDescription "jinkies jokies" 
 sc config jenkins start= auto
