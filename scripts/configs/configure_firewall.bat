dc08af99cf0c677f803d67139e9ef8228c117983
Blaz <blaz.pirc04@gmail.com>
2026-09-11 09:19:46 +0200

    mysql works

--- scripts/configs/configure_firewall.bat
+++ scripts/configs/configure_firewall.bat
@@ -14,5 +14,5 @@ netsh advfirewall firewall add rule name="Open Port 161 for SNMP" dir=in action=
 netsh advfirewall firewall add rule name="Closed port 445 for SMB" dir=in action=block protocol=TCP localport=445
 netsh advfirewall firewall add rule name="Closed port 139 for NetBIOS" dir=in action=block protocol=TCP localport=139
 netsh advfirewall firewall add rule name="Closed port 135 for NetBIOS" dir=in action=block protocol=TCP localport=135
-netsh advfirewall firewall add rule name="Closed Port 3389 for Remote Desktop" dir=in action=block protocol=TCP localport=3389
-netsh advfirewall firewall add rule name="Closed Port 3306 for MySQL" dir=in action=block protocol=TCP localport=3306
+netsh advfirewall firewall add rule name="Closed Port 3839 for Remote Desktop" dir=in action=block protocol=TCP localport=3839
+netsh advfirewall firewall add rule name="Closed Port 3360 for MySQL" dir=in action=block protocol=TCP localport=3360
